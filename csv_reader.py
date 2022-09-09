09236e919422fc5c8fc75b0a3fdec61661ad0ce7
5ABAYK41 <98584946+5ABAYK41@users.noreply.github.com>
2022-09-09 21:59:55 +0100

    Add files via upload

--- csv_reader.py
+++ csv_reader.py
@@ -1,6 +1,6 @@
 import csv
 
-def get_top_ranked_song(date):
+def get_song_detail_by_date(date):
     # open the csv file
     file = open('./csv/charts.csv')
     csvreader = csv.reader(file)
@@ -10,5 +10,25 @@ def get_top_ranked_song(date):
         if row[0] == date:
             rows.append(row)
     # close the csv file
+    file.close()
+    return rows
+
+def get_song_detail_by_rank(rank):
+    file = open('./csv/charts.csv')
+    csvreader = csv.reader(file)
+    rows = []
+    for row in csvreader:
+        # select the rows by the rank
+        if row[1] == rank:
+            rows.append(row)
+    file.close()
+    return rows
+
+def get_all_songs():
+    file = open('./csv/charts.csv')
+    csvreader = csv.reader(file)
+    rows = []
+    for row in csvreader:
+        rows.append(row)
     file.close()
     return rows
--- main.py
+++ main.py
@@ -1,19 +1,108 @@
 import os
 import csv_reader
+import sys
+
+TOP_FOR_RANKED_SONGS = 5
+TOP_FOR_WEEK_ON_BOARD = 10
 
 def clear_terminal():
     os.system('cls')
+
+# return to menu based on a keyboard input
+def return_menu(__func,args=False):
+    return_menu = input("Do you want to return menu?[Y/N]: ")
+    if return_menu == "Y" or return_menu == "y" or return_menu == "yes":
+        print_menu()
+    elif return_menu == "N" or return_menu == "n" or return_menu == "no":
+        if not args:
+            __func(args)
+        else:
+            __func(args)
+    else:
+        clear_terminal()
+        print('\x1b[0;39;43m' + 'OOPS! Invalid input' + '\x1b[0m')
+        if not args:
+            __func(True)
+        else:
+            __func(args,True)
+
+# get the rank of the songs list
+def get_rank(song):
+    rank = int(song[1])
+    return rank
+
+
 # display top ranked song of a give day
-def top_ranked():
-    clear_terminal()
-    print("Top ranked song")
+def top_ranked(date=False,no_clear_terminal=False):
+    if not no_clear_terminal:
+        clear_terminal()
+    print("Top Ranked Songs")
     print("---------------")
     print('')
-    date = input('Enter the required date (e.g:- 2021-11-06): ')
-    csv_reader.get_top_ranked_song(date)
+    if not date:
+        date = input('Enter the required date (e.g:- 2021-11-06): ')
+    else:
+        print('Enter the required date (e.g:- 2021-11-06):', date)
+    songs = csv_reader.get_song_detail_by_date(date)
+    songs.sort(key=get_rank)
+    for song in songs[0:TOP_FOR_RANKED_SONGS]:
+        print(song)
+    return_menu(top_ranked,date)
+
+def catch_unique(list_in):
+   # intilize an empty list
+   unq_list = []
+
+   # Check for elements
+   for x in list_in:
+      # check if exists in unq_list
+      if x not in unq_list:
+         unq_list.append(x)
+         # print list
+   return unq_list
+    
+def top_artist(no_clear_terminal=False):
+    if not no_clear_terminal:
+        clear_terminal()
+    print("Top Artists")
+    print("---------------")
+    print('')
+    artists = csv_reader.get_song_detail_by_rank("1")
+    artists_list = []
+    for artist in artists:
+        # print(artist)
+        artists_list.append(artist[3])
+
+    # get distict artists list
+    set_res = set(artists_list)
+    list_res = (list(set_res))
+    print(list_res)
+    return_menu(top_artist)
+
+# get the week on board of the songs list
+def get_week_on_board(song):
+    week_on_board = int(song[6])
+    return week_on_board
+
+def top_weekon_board(no_clear_terminal=False):
+    if not no_clear_terminal:
+        clear_terminal()
+    print("Top 10 Week On Board")
+    print("---------------")
+    print('')
+    songs = csv_reader.get_all_songs()
+    songs.sort(key=get_week_on_board)
+    for song in songs[0:TOP_FOR_WEEK_ON_BOARD]:
+        print(song)
+    return_menu(top_weekon_board)
+
+    
+
 
 # print menu
-def print_menu():
+def print_menu(no_clear_terminal=False):
+    if not no_clear_terminal:
+        clear_terminal()
     print("Top in Billboard!")
     print("-----------------")
     print("1. Top ranked song")
@@ -27,16 +116,16 @@ def print_menu():
     if op == '1':
         top_ranked()
     elif op == '2':
-        pass
+        top_artist()
     elif op == '3':
-        pass
+        top_weekon_board()
     elif op == '4':
         pass
     elif op == '5':
-        pass
+        sys.exit()
     else:
         clear_terminal()
         print('\x1b[0;39;43m' + 'OOPS! Invalid input' + '\x1b[0m')
-        op = print_menu()
+        op = print_menu(no_clear_terminal=True)
 
 print_menu()
